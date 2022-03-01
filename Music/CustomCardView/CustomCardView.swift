d01491c2aa8a278c29f08681ab14cfa090d78e07
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2022-03-01 17:19:32 -0300

    updating custom card view

--- Music/CustomCardView/CustomCardView.swift
+++ Music/CustomCardView/CustomCardView.swift
@@ -21,63 +21,148 @@ class CustomCardView: UIView {
     var containerTopConstraints: NSLayoutConstraint?
     var containerTrailingConstraints: NSLayoutConstraint?
     var containerBottomConstraints: NSLayoutConstraint?
-    var dataModel: CardViewModel?
+    var cardViewModel: CardViewModel?
     
     // MARK: - View setup
     
     lazy var cardContainerView: UIView = {
-        let cardContainerView = UIView()
-        cardContainerView.translatesAutoresizingMaskIntoConstraints = false
-        cardContainerView.layer.cornerRadius = 30
-        cardContainerView.clipsToBounds = true
-        cardContainerView.layer.shadowOpacity = 1
-        cardContainerView.layer.shadowOffset = CGSize(width: 0, height: -2)
-        cardContainerView.layer.shadowRadius = 20
-        return cardContainerView
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 30
+        view.clipsToBounds = true
+        view.layer.shadowOpacity = 1
+        view.layer.shadowOffset = CGSize(width: 0, height: -2)
+        view.layer.shadowRadius = 20
+        return view
     }()
     
     lazy var cardImageView: UIImageView = {
-        let cardImageView = UIImageView()
-        cardImageView.translatesAutoresizingMaskIntoConstraints = false
-        cardImageView.contentMode = .scaleAspectFill
-        cardImageView.backgroundColor = .black
-        return cardImageView
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .black
+        return imageView
     }()
     
     lazy var overlayView: UIView = {
-        let overlayView = UIView()
-        overlayView.translatesAutoresizingMaskIntoConstraints = false
-        overlayView.backgroundColor = UIColor.black.withAlphaComponent(0.6)
-        return overlayView
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        return view
     }()
     
     lazy var profileBorderView: UIView = {
-        let profileBorderView = UIView()
-        profileBorderView.translatesAutoresizingMaskIntoConstraints = false
-        profileBorderView.backgroundColor = .clear
-        profileBorderView.layer.borderWidth = 1
-        profileBorderView.layer.borderColor = UIColor.white.cgColor
-        profileBorderView.layer.cornerRadius = 25
-        return profileBorderView
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .clear
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.white.cgColor
+        view.layer.cornerRadius = 25
+        return view
     }()
     
     lazy var cardProfileImageView: UIImageView = {
-        let cardProfileImageView = UIImageView()
-        cardProfileImageView.translatesAutoresizingMaskIntoConstraints = false
-        cardProfileImageView.contentMode = .scaleAspectFill
-        cardProfileImageView.backgroundColor = .black
-        cardProfileImageView.clipsToBounds = true
-        cardProfileImageView.layer.cornerRadius = 20
-        return cardProfileImageView
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .black
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 20
+        return imageView
     }()
     
     lazy var addProfileImageButton: UIButton = {
-        let addProfileImageButton = UIButton()
-        addProfileImageButton.translatesAutoresizingMaskIntoConstraints = false
-        addProfileImageButton.backgroundColor = .white
-        addProfileImageButton.setBackgroundImage(UIImage(named: "plus"), for: .normal)
-        addProfileImageButton.layer.cornerRadius = 10
-        return addProfileImageButton
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .white
+        button.setBackgroundImage(UIImage(named: "plus"), for: .normal)
+        button.layer.cornerRadius = 10
+        return button
     }()
+    
+    lazy var cardCategoryTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 12, weight: .bold)
+        label.textColor = .white
+        return label
+    }()
+    
+    lazy var cardCategoryDateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 11, weight: .regular)
+        label.textColor = .white
+        return label
+    }()
+    
+    lazy var cardTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 31, weight: .bold)
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var likeAndTimeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var descriptionTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    init(viewMode: ViewMode, cardViewModel: CardViewModel) {
+        let frame: CGRect = CGRect.zero
+        super.init(frame: frame)
+        self.viewMode = viewMode
+        self.cardViewModel = cardViewModel
+        self.addSubview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubview() {
+        self.addSubview(self.cardContainerView)
+        self.cardContainerView.addSubview(self.cardImageView)
+        self.cardContainerView.addSubview(self.overlayView)
+        self.cardContainerView.addSubview(self.profileBorderView)
+        self.cardContainerView.addSubview(self.cardProfileImageView)
+        self.cardContainerView.addSubview(self.addProfileImageButton)
+        self.cardContainerView.addSubview(self.cardCategoryTitleLabel)
+        self.cardContainerView.addSubview(self.cardCategoryDateLabel)
+        self.cardContainerView.addSubview(self.cardTitleLabel)
+        self.cardContainerView.addSubview(self.likeAndTimeLabel)
+        self.cardContainerView.addSubview(self.descriptionTitleLabel)
+        self.updateLayout(for: self.viewMode ?? .card)
+    }
+    
+    private func updateLayout(for mode: ViewMode) {
+        switch mode {
+        case .full:
+            self.containerLeadingConstraints?.constant = 0
+            self.containerTopConstraints?.constant = 0
+            self.containerTrailingConstraints?.constant = 0
+            self.containerBottomConstraints?.constant = 0
+            self.descriptionTitleLabel.isHidden = false
+        case .card:
+            self.containerLeadingConstraints?.constant = 30
+            self.containerTopConstraints?.constant = 15
+            self.containerTrailingConstraints?.constant = -30
+            self.containerBottomConstraints?.constant = -15
+            self.descriptionTitleLabel.isHidden = true
+        }
+    }
 
 }
